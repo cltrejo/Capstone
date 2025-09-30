0d1d12e1f8cf14a112214b0a446984ea5b7338fc
Doritos0 <meyesblue@gmail.com>
2025-09-30 11:54:54 -0300

    asd

--- Trejo_Figueroa_Ruiz/Fase 2/frontend-o/src/components/Icons.jsx
+++ Trejo_Figueroa_Ruiz/Fase 2/frontend-o/src/components/Icons.jsx
@@ -3,9 +3,10 @@ export function Room() {
         <svg width="100%" height="auto" viewBox="0 0 400 300" style={{maxWidth: '52%'}}>
             <path 
                 d="M50,50 L350,50 L350,250 L280,250 L280,200 L220,200 L220,250 L50,250 Z" 
-                fill="#14B78F" 
+                fill="#99CA88" 
                 stroke="#081FF6" 
                 strokeWidth="4"
+                strokeOpacity="0.7"  // 70% de opacidad
             />
         </svg>
     )
